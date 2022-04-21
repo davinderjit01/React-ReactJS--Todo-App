416412c31e58521ec41a4c919de7bc90e77cb0d9
davinderjit01 <dkaur@ualberta.ca>
2022-04-21 09:29:44 -0600

    Todo App using useState useEffect

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
 //ReactDOM takes two arguments(HTML code and HTML element)
 //This func is used to display html code in THML element
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  //Render the whole React App into the element with id=root.
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
